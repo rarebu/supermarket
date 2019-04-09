862ce4eaa392076abd2ffe36e9554cb749ef58dd
rarebu <rafthebee@gmail.com>
2019-04-09 15:11:29 +0200

    next step: serving_customer because serving returns after 10 and not 100 seconds

--- supermarket/main.py
+++ supermarket/main.py
@@ -1,60 +1,57 @@
-import time, heapq, threading, queue, logging
-from collections import deque
-from datetime import datetime
+import time
+import heapq
+import threading
+import queue
+import logging
+from datetime import datetime, timedelta
 
 
-class Station_Customer_view:
-    def __init__(self, stationType, itemCount, maxQueueSize):
-        self.stationType = stationType
-        self.itemCount = itemCount
-        self.maxQueueSize = maxQueueSize
+class Station_customer_view:
+    def __init__(self, station_type, item_count, max_queue_size):
+        self.station_type = station_type
+        self.item_count = item_count
+        self.max_queue_size = max_queue_size
 
 
 class Customer:
     def __init__(self, type_id):
 
         if type_id == 1:    # Type A
-            self.baecker = Station_Customer_view("Bäcker", 10, 10)
-            self.wursttheke = Station_Customer_view("Wursttheke", 5, 10)
-            self.kasestheke = Station_Customer_view("Käsetheke", 3, 5)
-            self.kasse = Station_Customer_view("Kasse", 30, 20)
+            self.baecker = Station_customer_view("Bäcker", 10, 10)
+            self.wursttheke = Station_customer_view("Wursttheke", 5, 10)
+            self.kasestheke = Station_customer_view("Käsetheke", 3, 5)
+            self.kasse = Station_customer_view("Kasse", 30, 20)
             self.type_id = 'A'
             self.id = 0
 
         elif type_id == 2:    # Type B
-            self.baecker = Station_Customer_view("Bäcker", 3, 20)
-            self.wursttheke = Station_Customer_view("Wursttheke", 2, 5)
-            self.kasse = Station_Customer_view("Kasse", 3, 20)
+            self.baecker = Station_customer_view("Bäcker", 3, 20)
+            self.wursttheke = Station_customer_view("Wursttheke", 2, 5)
+            self.kasse = Station_customer_view("Kasse", 3, 20)
             self.type_id ='B'
             self.id = 0
 
 
 class Shop:
-    def serving_customer(queue, serveTime, lastServed):
-        now = datetime.now()
-        diff = now - lastServed
-        print("serving() diff.seconds " + str(diff.seconds) + " serveTime " + str(serveTime))
+    def serving_customer(queue, serve_time, last_served, shop_name):
+        return Shop.serving_item(queue, serve_time, last_served, shop_name)
 
-        if (diff.seconds >= serveTime):
-            print("serving() yes, diff passed")
-            print("serving() queue: ", queue.qsize())
+    def serving_item(queue, serve_time, last_served, shop_name):
+        now = datetime.now()
+        diff = now - last_served
+
+        if (diff.seconds >= serve_time):
             if not queue.empty():
                 customer = queue.get()
-                print("serving() customer " + str(customer.type_id)+str(customer.id))
-                logger.info("Shop serving customer " + str(customer.type_id) + str(customer.id))
-                # with open("supermarkt_station.txt", "a") as myfile:
-                #    myfile.write("Bäcker serving customer " + str(customer.type_id)+str(customer.id) + "\n")
-                # time.sleep(customer.baecker.itemCount * serveTime)
-                # with open("supermarkt_station.txt", "a") as myfile:
-                #    myfile.write("Bäcker finished customer " + str(customer.type_id) + str(customer.id) + "\n")
-                return True
-            return False
+                tmpstr = str(customer.type_id) + str(customer.id)
+                logger.info(shop_name + " serving customer " + tmpstr)
+                return customer.baecker.item_count * serve_time, tmpstr
+            return 0, ""
         else:
-            print("serving() no, diff not passed")
-            return False
+            return 0, ""
 
     def customer_joining_queue(self, customer):
-        super.addCustomer(queue, customer)
+        super.add_customer(queue, customer)
 
     def poll_server(self, station):
         while not station.serving():
@@ -62,103 +59,91 @@ class Shop:
 
 
 class Baecker:
-    serveTime = 8
+    serve_time = 10
 
     def __init__(self):
-        print("State init")
         self.queue = queue.Queue()
-        self.lastServed = datetime(2000, 1, 1)
+        self.last_served = datetime(2000, 1, 1)
 
     def serving(self):
-        if Shop.serving_customer(self.queue, self.serveTime, self.lastServed):
-            self.lastServed = datetime.now()
-            return True
-        else:
-            return False
+        tmp = Shop.serving_customer(self.queue, self.serve_time, self.last_served, "Bäcker")
+        if tmp[0] != 0:
+            self.last_served = datetime.now()
+        return tmp
 
-    def getQueueSize(self, queue):
+    def get_queue_size(self, queue):
         return queue.len()
 
-    def addCustomer(self, customer):
-        print("State added")
+    def add_customer(self, customer):
         logger.info("Bäcker adding customer " + str(customer.type_id) + str(customer.id))
-        # with open("supermarkt_station.txt", "a") as myfile:
-        #    myfile.write("Bäcker adding customer " + str(customer.type_id) + str(customer.id) + "\n")
         self.queue.put(customer)
 
 
 class Wursttheke:
-    serveTime = 30
+    serve_time = 30
 
     def __init__(self):
         self.queue = queue.Queue()
-        self.lastServed = datetime(2000, 1, 1)
+        self.last_served = datetime(2000, 1, 1)
 
     def serving(self):
-        if Shop.serving_customer(self.queue, self.serveTime, self.lastServed):
-            self.lastServed = datetime.now()
-            return True
-        else:
-            return False
+        tmp = Shop.serving_customer(self.queue, self.serve_time, self.last_served)
+        if tmp != 0:
+            self.last_served = datetime.now()
+        return tmp
 
-    def getQueueSize(self, queue):
+    def get_queue_size(self, queue):
         return queue.len()
 
-    def addCustomer(self, customer):
+    def add_customer(self, customer):
         logger.info("Wursttheke adding customer " + str(customer.type_id) + str(customer.id))
-        # with open("supermarkt_station.txt", "a") as myfile:
-        #    myfile.write("Wursttheke adding customer " + str(customer.type_id) + str(customer.id) + "\n")
         self.queue.put(customer)
 
 
 class Kaesetheke:
-    serveTime = 60
+    serve_time = 60
 
     def __init__(self):
         self.queue = queue.Queue()
-        self.lastServed = datetime(2000, 1, 1)
+        self.last_served = datetime(2000, 1, 1)
 
     def serving(self):
-        if Shop.serving_customer(self.queue, self.serveTime, self.lastServed):
-            self.lastServed = datetime.now()
-            return True
-        else:
-            return False
+        tmp = Shop.serving_customer(self.queue, self.serve_time, self.last_served)
+        if tmp != 0:
+            self.last_served = datetime.now()
+        return tmp
 
-    def getQueueSize(self, queue):
+    def get_queue_size(self, queue):
         return queue.len()
 
-    def addCustomer(self, customer):
+    def add_customer(self, customer):
         logger.info("Käsetheke adding customer " + str(customer.type_id) + str(customer.id))
-        # with open("supermarkt_station.txt", "a") as myfile:
-        #    myfile.write("Käsetheke adding customer " + str(customer.type_id) + str(customer.id) + "\n")
         self.queue.put(customer)
 
 
 class Kasse:
-    serveTime = 60
+    serve_time = 5
 
     def __init__(self):
         self.queue = queue.Queue()
-        self.lastServed = datetime(2000, 1, 1)
+        self.last_served = datetime(2000, 1, 1)
 
     def serving(self):
-        if Shop.serving_customer(self.queue, self.serveTime, self.lastServed):
-            self.lastServed = datetime.now()
-            return True
-        else:
-            return False
+        tmp = Shop.serving_customer(self.queue, self.serve_time, self.last_served)
+        if tmp != 0:
+            self.last_served = datetime.now()
+        return tmp
 
-    def getQueueSize(self, queue):
+    def get_queue_size(self, queue):
         return queue.len()
 
-    def addCustomer(self, customer):
-        # with open("supermarkt_station.txt", "a") as myfile:
-        #     myfile.write("Kasse adding customer " + str(customer.type_id) + str(customer.id) + "\n")
+    def add_customer(self, customer):
         logger.info("Kasse adding customer " + str(customer.type_id) + str(customer.id))
         self.queue.put(customer)
 
 
+heap = []
+heapq.heapify(heap)
 
 logger = logging.getLogger('msglog')
 logger.setLevel(logging.INFO)
@@ -167,42 +152,87 @@ handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-
 logger.info('Test Start')
 
 t1c1 = Customer(1)
-t1c1.id = 1
-
 t1c2 = Customer(1)
+t1c3 = Customer(1)
+t1c4 = Customer(1)
+t1c5 = Customer(1)
+t1c6 = Customer(1)
+t1c7 = Customer(1)
+t1c8 = Customer(1)
+t1c9= Customer(1)
+t1c10 = Customer(1)
+t1c1.id = 1
 t1c2.id = 2
+t1c3.id = 3
+t1c4.id = 4
+t1c5.id = 5
+t1c6.id = 6
+t1c7.id = 7
+t1c8.id = 8
+t1c9.id = 9
+t1c10.id = 10
 
 t2c1 = Customer(2)
+t2c2 = Customer(2)
+t2c3 = Customer(2)
+t2c4 = Customer(2)
+t2c5 = Customer(2)
+t2c6 = Customer(2)
+t2c7 = Customer(2)
+t2c8 = Customer(2)
+t2c9 = Customer(2)
+t2c10 = Customer(2)
 t2c1.id = 1
+t2c2.id = 2
+t2c3.id = 3
+t2c4.id = 4
+t2c5.id = 5
+t2c6.id = 6
+t2c7.id = 7
+t2c8.id = 8
+t2c9.id = 9
+t2c10.id = 10
 
 baecker = Baecker()
+kaesetheke = Kaesetheke()
+wursttheke = Wursttheke()
+kasse = Kasse()
 
-# baecker.serving()
-baecker.addCustomer(t1c1)
-baecker.addCustomer(t1c2)
+baecker.add_customer(t1c1)
+baecker.add_customer(t1c2)
+baecker.add_customer(t1c3)
+baecker.add_customer(t1c4)
+baecker.add_customer(t1c5)
+baecker.add_customer(t1c6)
+baecker.add_customer(t1c7)
+baecker.add_customer(t1c8)
+baecker.add_customer(t1c9)
+baecker.add_customer(t1c10)
+baecker.add_customer(t2c1)
+baecker.add_customer(t2c2)
+baecker.add_customer(t2c3)
+baecker.add_customer(t2c4)
+baecker.add_customer(t2c5)
+baecker.add_customer(t2c6)
+baecker.add_customer(t2c7)
+baecker.add_customer(t2c8)
+baecker.add_customer(t2c9)
+baecker.add_customer(t2c10)
+
 while True:
-    print("serving() returns: " + str(baecker.serving()))
+    tmp = baecker.serving()
+    if tmp[0] != 0:
+        heapq.heappush(heap, (datetime.now() + timedelta(0, tmp[0]), tmp[1]))
+        print(list(heap))
     time.sleep(2)
-# with open("supermarkt_station.txt", "a") as myfile:
-#     myfile.write("Bäcker finished customer " + str(customer.type_id) + str(customer.id) + "\n")
-
-time.sleep(5)
-
-baecker.addCustomer(t1c2)
-print(baecker.serving())
-
 
 
 # t = threading.Thread(target=Baecker.start_serving)
 # t.daemon = True
 # t.start()
-# Shop.serving_customer(self.serveTime, self.queue)
+# Shop.serving_customer(self.serve_time, self.queue)
 
 # Shop.poll_server(baecker) #nur bei threads
-
-
-# time.sleep(100)
